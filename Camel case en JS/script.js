6d5d286dcf470f7b1fbcab4e447d7b73d37f1b42
Diego <ogeid_to@hotmail.com>
2021-10-16 13:03:48 -0600

    Avance en curso

---
+++ Camel case en JS/script.js
@@ -0,0 +1,3 @@
+//Camel case es la regla de escritura de palabras en JS, que aplica también a la declaración de variables. Se inicia con letras minúsculas y cada nueva palabra que vaya pegada para diferenciarse empezará con mayúscula.
+//Ejemplo:
+var holaSoyUnCamelCase = true;
